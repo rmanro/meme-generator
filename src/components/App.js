2f7dd591a9e85be9f1f94362406b7b610f55ac59
Ryan Manro <rmanro@gmail.com>
2018-05-29 17:18:05 -0700

    file upload working

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,16 @@ export default class App extends Component {
     this.setState({ image: target.value });
   }
 
+  handleUpload({ target }) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(target.files[0]);
+
+    reader.onload = () => {
+      this.setState({ image: reader.result });
+    };
+  }
+
   render() {
     const { image } = this.state;
 
@@ -27,6 +37,15 @@ export default class App extends Component {
             <label>
               Image URL:
               <input onChange={event => this.handleImageUrl(event)}/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Upload Image:
+              <input
+                type="file"
+                onChange={event => this.handleUpload(event)}
+              />
             </label>
           </div>
         </section>
